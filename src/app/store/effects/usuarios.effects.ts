b70a9025fe097aa4636b155c29c2b68cd39d45d3
israel <israel.bejarano.a@gmail.com>
2019-04-26 14:45:51 +0200

    obtener el error

--- src/app/store/effects/usuarios.effects.ts
+++ src/app/store/effects/usuarios.effects.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Actions, ofType, Effect } from '@ngrx/effects';
 import * as usuariosActions from '../actions';
-import { map, switchMap } from 'rxjs/operators';
+import {of} from 'rxjs';
+import { map, switchMap, catchError } from 'rxjs/operators';
 import { UsuarioService } from '../../services/usuario.service';
 
 @Injectable()
@@ -12,6 +13,7 @@ export class UsuariosEffects {
     cargarUsuarios$ = this.actions$.pipe(ofType(usuariosActions.CARGAR_USUARIOS))
         .pipe(switchMap(() => {
             return this.usuarioService.getUsers()
-                .pipe(map(usuarios => new usuariosActions.CargarUsuariosSuccess(usuarios)));
+                .pipe(map(usuarios => new usuariosActions.CargarUsuariosSuccess(usuarios)),
+                catchError(err => of(new usuariosActions.CargarUsuariosFail(err))));
         }));
 }
